dc36845837f6177e6f5c8a884b688af5ca324c47
Adrian <adriangonz457@gmail.com>
2021-07-16 16:58:10 -0400

    Added requirements page.

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def login():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route("/requirements")
+def requirements():
+    return render_template("requirements.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
