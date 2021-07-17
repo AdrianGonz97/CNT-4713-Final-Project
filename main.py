6b7cf3179d23b2caca3cf3568b48e2abbfc764a0
Adrian <adriangonz457@gmail.com>
2021-07-16 22:17:48 -0400

    Added registration form validation and logic

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def login():
         password = request.form["password"]
         # creates a session for the user
         session["user"] = username
+        flash("You have successfully logged in!")
         return redirect(url_for("dashboard"))
     else:
         # route to dashboard if user is already logged in 
@@ -46,9 +47,27 @@ def logout():
     return redirect(url_for("login"))
 
 # user registration page
-@app.route("/register")
+@app.route("/register", methods=['POST', "GET"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        password = request.form["password"]
+        repassword = request.form["repassword"]
+        # if the passwords match..
+        if repassword == password:
+            username = request.form["username"]
+            # and if the pass or username is not blank or empty
+            if password and password.strip() and username and username.strip():
+                # perform the registration
+                flash("You have successfully registered!")
+                return redirect(url_for("login"))
+            else:
+                flash("The passwords and usernames cannot be blank!")
+                return redirect(url_for("register"))
+        else: # if they don't match..
+            flash("The passwords do not match!")
+            return redirect(url_for("register"))
+    else: # otherwise, send to page
+        return render_template("register.html")
 
 # user dashboard page
 @app.route("/dashboard")
@@ -59,6 +78,7 @@ def dashboard():
         user = session["user"]
         return render_template("dashboard.html", username=user)
     else: # otherwise, send to login
+        flash("You need to be logged in to access the dashboard!")
         return redirect(url_for("login"))
 
 # routes to the about page
